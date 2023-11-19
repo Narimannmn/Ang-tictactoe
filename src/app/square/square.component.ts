834e47031ca23c5dfb503971fccf04d4b4420c20
Narimannmn <138319759+Narimannmn@users.noreply.github.com>
2023-11-19 23:27:39 +0600

    initial commit

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-square',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './square.component.html',
+  styleUrl: './square.component.css'
+})
+export class SquareComponent {
+
+}
